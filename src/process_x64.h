e9d37bfbf4c3a24689403ba33d7f48ca2d8af673
Seoyun <seoyun1062@outlook.com>
2026-08-03 19:19:24 +0800

    Add assembly implementation header

---
+++ src/process_x64.h
@@ -0,0 +1,14 @@
+#ifndef PROCESS_X64_H_
+#define PROCESS_X64_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void distance_x64(int n, const float* x1, const float* x2, const float* y1, const float* y2, float* z);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // PROCESS_X64_H_
